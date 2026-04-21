332191872f86657cc6bbe1ff36c003fb56413fb5
itzcloudycuz <162707895+itzcloudycuz@users.noreply.github.com>
2026-04-21 13:59:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,25 @@
-// 🔥 Firebase config will go here later
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Example:
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyD4aQWh8yc8bA6XQm5XWTq0EEQPKGynqBI",
+  authDomain: "poker-ledger-pwa.firebaseapp.com",
+  projectId: "poker-ledger-pwa",
+  storageBucket: "poker-ledger-pwa.firebasestorage.app",
+  messagingSenderId: "865251433349",
+  appId: "1:865251433349:web:32fa9ec10555dd6f8102ec",
+  measurementId: "G-S82B8G02N9"
+};
 
-// const firebaseConfig = {
-//   apiKey: "YOUR_KEY",
-//   authDomain: "YOUR_APP.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-// };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
+window.db = db;
+window.doc = doc;
+window.setDoc = setDoc;
+window.onSnapshot = onSnapshot;
