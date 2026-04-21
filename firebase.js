abe36302086bbac0dec8a4de06dced0c0b19acbe
itzcloudycuz <162707895+itzcloudycuz@users.noreply.github.com>
2026-04-21 13:02:10 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// 🔥 Firebase config will go here later
+
+// Example:
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "YOUR_KEY",
+//   authDomain: "YOUR_APP.firebaseapp.com",
+//   projectId: "YOUR_PROJECT_ID",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
